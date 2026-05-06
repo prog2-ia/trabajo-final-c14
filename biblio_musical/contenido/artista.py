4793ef34952e917497793d4408d977b87de5ce55
sandracrevillen <scc106@alu.ua.es>
2026-05-06 16:11:08 +0200

    añadir funcion a artista.py

--- biblio_musical/contenido/artista.py
+++ biblio_musical/contenido/artista.py
@@ -6,8 +6,13 @@ class Artista:
         self._albums = []
 
     def agregar_album(self, album):
-        """Añade un álbum al artista."""
-        self._albums.append(album)
+        """Añade un album"""
+        if album not in self._albums:
+            self._albums.append(album)
+            # Actualizamos el atributo del album para que apunte a este artista
+            album._artista = self 
+        else:
+            print(f"El album '{album.titulo}' ya esta en la coleccion.")
 
     def buscar_album(self, titulo):
         for a in self._albums:
@@ -18,8 +23,12 @@ class Artista:
     def mostrar_albums(self):
         """Muestra todos los álbumes del artista."""
         print(f"Artista: {self.nombre}")
-        for a in self._albums:
+        for a in sorted(self._albums):
             print(a)
+
+    def total_pistas(self):
+        """Calcula cuantas pistas tiene el artista en total."""
+        return sum(len(a._pistas) for a in self._albums)
 
     def reproducir(self):
         """Reproduce todos los álbumes del artista (polimorfismo)."""
