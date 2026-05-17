f38afa6becf2a3ba4d97ca46e40c4811df197f97
sandracrevillen <scc106@alu.ua.es>
2026-05-17 11:31:25 +0200

    mejoras en usuario admin

--- biblio_musical/usuarios/usuario_administrador.py
+++ biblio_musical/usuarios/usuario_administrador.py
@@ -16,9 +16,20 @@ class UsuarioAdministrador(Usuario):
             print(f"Playlist {playlist.titulo} eliminada del usuario {usuario.nombre}")
         else:
             print(f"La playlist no se encuentra en el perfil de {usuario.nombre}")
+    
+    def banear_usuario(self, usuario):
+        usuario.baneado = True
+        print(f"{usuario.nombre} ha sido baneado")
+
+    def ver_total_playlists(self, usuario):
+        return len(usuario._playlists)
+    
+    def eliminar_todas_playlists(self, usuario):
+        usuario._playlists.clear()
 
     def __str__(self) -> str:
         return f"Usuario Administrador: {self.nombre}"
 
     def __repr__(self) -> str:
         return f"UsuarioAdministrador(nombre='{self.nombre}')"
+    
