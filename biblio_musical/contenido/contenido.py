d7ab171944040f0fc64c6baa414f36f9ce476572
sandracrevillen <scc106@alu.ua.es>
2026-05-24 17:03:47 +0200

    comentada la clase contenido

--- biblio_musical/contenido/contenido.py
+++ biblio_musical/contenido/contenido.py
@@ -4,15 +4,27 @@ class Contenido(ABC):
     """Clase abstracta base para cualquier contenido musical."""
 
     def __init__(self, titulo: str) -> None:
+        """
+        Inicializa la clase base abstracta con un título.
+
+        Args:
+            titulo (str): El título o nombre del contenido musical.
+        """
         self.titulo = titulo
 
     @abstractmethod
     def reproducir(self) -> None:
-        """Cada contenido debe definir cómo se reproduce."""
+        """
+        Cada contenido debe definir cómo se reproduce.
+        Este método es abstracto y debe ser implementado obligatoriamente por cualquier subclase que herede de Contenido.
+        
+        """
         pass
 
     def __str__(self) -> str:
+        """Devuelve una representación del contenido en formato de cadena de texto."""
         return f"Contenido: {self.titulo}"
 
     def __repr__(self) -> str:
+        """Devuelve una representación técnica del objeto contenido."""
         return f"Contenido(titulo='{self.titulo}')"
